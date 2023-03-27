b29ccf439ca5304fbb5e39179ad8b0bbb942853c
sunny-potato <sun_hee0210@naver.com>
2023-03-27 16:17:08 +0200

    copy and paste text

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Random password generator
 
 1. addEventListener("input") vs addEventListener("change")
+
    - addEventListener("input", (event) => {} ) === oninput = (event) => {};
      :event is fired when the element's value is changed
    - addEventListener("change", (event) => {} ) === onchange = (event) => {};
      : change event is fired when element's value is finished changing (= when losing focus)
+
+2. navigator.clipboard
--- index.js
+++ index.js
@@ -11,40 +11,56 @@ const uppercase = document.querySelector("#uppercase");
 const numbers = document.querySelector("#numbers");
 const symbols = document.querySelector("#symbols");
 const options = [length_range, length_number, uppercase, numbers, symbols];
-
 let characters_list = [characters.lowercase];
+let password_length = 10;
+// console.log(characters_list);
 options.forEach((option) => {
   option.addEventListener("input", (event) => {
+    // console.log(characters_list);
     if (event.target.checked) {
+      // console.log(password_length);
       characters_list.push(characters[event.target.id]);
       const characters_string = characters_list.join("");
-      update_password(50, characters_string.length, characters_string);
+      update_password(
+        password_length,
+        characters_string.length,
+        characters_string
+      );
     } else {
       for (index in characters_list) {
         if (characters_list[index] === characters[event.target.id]) {
           characters_list.splice(index, 1);
           const characters_string = characters_list.join("");
-          update_password(50, characters_string.length, characters_string);
+          update_password(
+            password_length,
+            characters_string.length,
+            characters_string
+          );
         }
       }
     }
-    if (event.target.id === "length_range") {
-      length_number.value = event.target.value;
-    }
-    if (event.target.id === "length_number") {
-      length_range.value = event.target.value;
+    if (
+      event.target.id === "length_range" ||
+      event.target.id === "length_number"
+    ) {
+      password_length = Number(event.target.value);
+      const characters_string = characters_list.join("");
+      update_password(
+        password_length,
+        characters_string.length,
+        characters_string
+      );
     }
   });
 });
 
 function update_password(length, max, characters) {
   const generated_password = generate_password(length, max, characters);
-  display_password(generated_password);
+  add_password(generated_password);
   update_password_length(generated_password.length);
 }
-
-function display_password(password) {
-  const display_password = document.querySelector(".display_password");
+const display_password = document.querySelector(".display_password");
+function add_password(password) {
   display_password.textContent = `${password}`;
 }
 
@@ -63,12 +79,44 @@ function generate_password(length, max, characters) {
   return password.join("");
 }
 
-const defaul_password = update_password(
-  10,
+const default_password = update_password(
+  password_length,
   characters.lowercase.length,
   characters.lowercase
 );
 
 window.addEventListener("load", () => {
-  defaul_password;
+  update_password(
+    password_length,
+    characters.lowercase.length,
+    characters.lowercase
+  );
 });
+
+const refresh_button = document.querySelector(".refresh_button");
+refresh_button.addEventListener("click", () => {
+  characters_list = [characters.lowercase];
+  const chekced_list = [uppercase, numbers, symbols];
+  chekced_list.forEach((each) => (each.checked = false));
+  password_length = 10;
+  update_password(
+    password_length,
+    characters.lowercase.length,
+    characters.lowercase
+  );
+});
+
+const copy_button = document.querySelector(".copy_button");
+copy_button.addEventListener("click", copy_password);
+async function copy_password() {
+  console.log(display_password.textContent);
+  const clip_text = await navigator.clipboard.writeText(
+    display_password.textContent
+  );
+  try {
+    const paste_text = await navigator.clipboard.readText(clip_text);
+    console.log(paste_text);
+  } catch (error) {
+    console.log(error);
+  }
+}
